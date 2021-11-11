69acdaf082e994e5dece21f4b22c3bd6f07f1800
RayMiles94 <Raouf.Jabri@samsa-it.de>
2021-11-11 09:15:35 +0100

    init commit

---
+++ src/test/java/sesame/workshop/sesame/workshop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sesame.workshop.sesame.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
