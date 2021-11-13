a141acf472360d7c5d6a0d6e9fa362fe0701edbf
RayMiles94 <natsuboy94@gmail.com>
2021-11-13 11:32:52 +0100

    [ADD] rest package and @http://localhost:4500/

--- src/main/java/sesame/workshop/sesame/workshop/DAO/ClientRepo.java
+++ src/main/java/sesame/workshop/sesame/workshop/DAO/ClientRepo.java
@@ -2,8 +2,11 @@ package sesame.workshop.sesame.workshop.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import sesame.workshop.sesame.workshop.entitie.Client;
 
+import sesame.workshop.sesame.workshop.entitie.Client;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
 public interface ClientRepo extends JpaRepository<Client, Long> {
 
 }
---
+++ src/main/java/sesame/workshop/sesame/workshop/controller/IndexController.java
@@ -0,0 +1,13 @@
+package sesame.workshop.sesame.workshop.controller;
+
+import java.util.ArrayList;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class IndexController {
+	
+	
+
+}
